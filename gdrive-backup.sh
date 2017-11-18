f4b2791919ff3fce3ce69c1452f23292e27784ec
fxleblanc <felix.leblanc1305@gmail.com>
2017-11-18 13:30:24 -0500

    Check if gdrive client is installed

--- gdrive-backup.sh
+++ gdrive-backup.sh
@@ -13,3 +13,10 @@ tar -cvf $archive_filename $1
 # Encrypting archive
 echo "Encrypting archive..."
 gpg --output $encrypted_filename --encrypt --recipient felix.leblanc1305@gmail.com $archive_filename
+
+# Check if gdrive client is there
+if [ ! -f /usr/bin/gdrive ]
+then
+    echo "You must install the google drive command line client"
+    exit 1
+fi
