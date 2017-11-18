7222eca74bea030ef0eb3ed9f67de1777d40807e
fxleblanc <felix.leblanc1305@gmail.com>
2017-11-18 13:13:07 -0500

    archive_filename variable

--- gdrive-backup.sh
+++ gdrive-backup.sh
@@ -3,7 +3,8 @@
 # Backup folder on google drive
 #
 folder=$1
+archive_filename=/tmp/gdrive-backup-archive-$(date -u +%s).tar
 
 # Create archive
 echo "Creating archive..."
-tar -cvf /tmp/gdrive-backup-archive-$(date -u +%s).tar $1
+tar -cvf $archive_filename $1
