b7de66dcc0e84f5096695d4b15a48302139765e2
fxleblanc <felix.leblanc1305@gmail.com>
2017-11-18 13:19:48 -0500

    Encrypt archive

--- gdrive-backup.sh
+++ gdrive-backup.sh
@@ -4,7 +4,12 @@
 #
 folder=$1
 archive_filename=/tmp/gdrive-backup-archive-$(date -u +%s).tar
+encrypted_filename="$archive_filename".gpg
 
 # Create archive
 echo "Creating archive..."
 tar -cvf $archive_filename $1
+
+# Encrypting archive
+echo "Encrypting archive..."
+gpg --output $encrypted_filename --encrypt --recipient felix.leblanc1305@gmail.com $archive_filename
