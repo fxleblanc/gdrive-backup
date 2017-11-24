0b4728b519f27814f3bfd269630f6372549ff778
fxleblanc <felix.leblanc1305@gmail.com>
2017-11-23 19:19:58 -0500

    cd in the folder specified in arguments

--- gdrive-backup.sh
+++ gdrive-backup.sh
@@ -2,13 +2,13 @@
 #
 # Backup folder on google drive
 #
-folder=$1
 archive_filename=/tmp/gdrive-backup-archive-$(date -u +%s).tar
 encrypted_filename="$archive_filename".gpg
 
 # Create archive
 echo "Creating archive..."
-tar -cvf $archive_filename $1
+cd $1
+tar -cvf $archive_filename .
 
 # Encrypting archive
 echo "Encrypting archive..."
