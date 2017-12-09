998d32d23668f9153c8ab9cb0852264037a1ff68
fxleblanc <felix.leblanc1305@gmail.com>
2017-12-09 11:03:44 -0500

    Clean backups in main backup script
    clean_backups function with number of backups to keep in parameters

--- gdrive-backup.sh
+++ gdrive-backup.sh
@@ -5,6 +5,37 @@
 archive_filename=/tmp/gdrive-backup-archive-$(date -u +%s).tar
 encrypted_filename="$archive_filename".gpg
 
+# Clean old backups function
+# $1: Number of backups to keep
+clean_backups () {
+    done=false
+    while [ "$done" = false ]
+    do
+        # Find the list of encrypted backups
+        backups=$(gdrive list | grep gpg | cut -d ' ' -f15)
+
+        count=$(printf "%s\n" "$backups" | wc -l)
+        echo "$count backups left"
+
+        if [ $count -gt $1 ]
+        then
+            # Find the dates of the listed backups
+            dates=$(printf "%s\n" "$backups" | cut -d '.' -f1 | cut -d '-' -f4)
+
+            # Find the oldest backup
+            oldest=$(printf "%s\n" "$dates" | sort -g | head -n1)
+
+            # Find the oldest backup's Google drive ID
+            oldest_gdrive_id=$(gdrive list | grep $oldest | cut -d " " -f1)
+
+            # Delete oldest backup
+            gdrive delete $oldest_gdrive_id
+        else
+            done=true
+        fi
+    done
+}
+
 # Create archive
 echo "Creating archive..."
 cd $1
@@ -24,3 +55,7 @@ fi
 # Upload to google drive
 echo "Uploading..."
 gdrive upload $encrypted_filename
+
+# Clean up old backups
+echo "Deleting old backups"
+clean_backups 2
